37411ccd8e0aa3567dfc6b78250561a71cd91b61
aleksei-shvetz <al.sv.dev@gmail.com>
2024-05-25 21:23:04 +0300

    Finish environment setting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,6 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import config from "eslint-config-standard";
 
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
+  ...[].concat(config),
 ];
